f5534b14843833e5d7f76f65c811c259255c5634
WH03 <35065229+WH03@users.noreply.github.com>
2023-06-09 15:37:22 +0800

    修改css2d

--- vite-three-project/07-css2d.js
+++ vite-three-project/07-css2d.js
@@ -1,13 +1,19 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
+import {
+    OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls.js';
+import {
+    CSS2DRenderer,
+    CSS2DObject
+} from 'three/examples/jsm/renderers/CSS2DRenderer.js';
 
+// console.log(666, THREE.REVISION);
 
 let scene, camera, renderer;
 let axesHelper;
 let controls;
 let hemisPhereLight, directionalLight;
-let sphereGeometry, sphereMaterial, earth, moon, earthLabel, moonLabel;
+let sphereGeometry, sphereMaterial, earth, moon, earthLabel, moonLabel,textLabel;
 let texture;
 
 let clock = new THREE.Clock();
@@ -18,25 +24,26 @@ let labelRenderer;
 
 
 function init() {
-
-    initCamera();
     initScene();
-    initAxesHelper();
-    createCSS2DRenderer();
-    initRenderer();
-    initControls();
+    initCamera();
+
+    initLight();
+    // initShadow();
+
+
+    // initAxesHelper();
 
 
 
     createEarth();
     createMoon();
+    createEarthLabel();
 
+    initRenderer();
 
+    // createCSS2DRenderer();
 
-    initLight();
-    initShadow();
-
-
+    initControls();
 }
 
 function initScene() {
@@ -46,16 +53,32 @@ function initScene() {
 
 function initCamera() {
     camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 3000);
-    camera.position.set(10, 10, 10);
+    camera.position.set(100, 100, 100);
     camera.lookAt(0, 0, 0);
 };
 
 function initRenderer() {
-    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     // renderer.setClearColor ('#B0E0E6');
     document.body.appendChild(renderer.domElement);
+
+
+    labelRenderer = new CSS2DRenderer();
+    // labelRenderer.domElement.style.pointerEvents = 'none';
+    labelRenderer.domElement.style.pointerEvents = 'none';
+    // 设置渲染尺寸
+    labelRenderer.setSize(window.innerWidth, window.innerHeight);
+    // 设置绝对定位，脱离文档流
+    labelRenderer.domElement.style.position = 'absolute';
+    labelRenderer.domElement.style.top = '0px';
+    document.body.appendChild(labelRenderer.domElement);
+
+
+
 };
 
 function initAxesHelper() {
@@ -74,8 +97,8 @@ function initLight() {
     directionalLight.position.set(5, 5, 5);
     scene.add(directionalLight);
 
-    const directionalLightHelp = new THREE.DirectionalLightHelper(directionalLight, 5);
-    scene.add(directionalLightHelp);
+    // const directionalLightHelp = new THREE.DirectionalLightHelper(directionalLight, 5);
+    // scene.add(directionalLightHelp);
 }
 
 // 创建地球
@@ -118,7 +141,7 @@ function createMoon() {
 // 创建CSS2DRenderer
 function createCSS2DRenderer() {
     labelRenderer = new CSS2DRenderer();
-    // labelRenderer.domElement.style.pointerEvents = 'none';
+    labelRenderer.domElement.style.pointerEvents = 'none';
     // 设置渲染尺寸
     labelRenderer.setSize(window.innerWidth, window.innerHeight);
     // 设置绝对定位，脱离文档流
@@ -130,33 +153,45 @@ function createCSS2DRenderer() {
 // 创建css2DObject
 function createEarthLabel() {
     earthLabel = document.createElement('div');
-    // earthLabel.style.pointerEvents = 'auto';
-    earthLabel.className = 'textLabel2d';
+    earthLabel.style.pointerEvents = 'auto';
+    earthLabel.className = 'textLabel';
     earthLabel.textContent = "EARTH";
     earthLabel.style.color = 'aqua'
-    let textLabel3 = new CSS2DObject(earthLabel);
-    textLabel3.position.set(0, 1.5, 0);
-    // earth.add(textLabel3);
-    scene.add(textLabel3);
+    earthLabel.style.cursor = "pointer"
+    earthLabel.style.zIndex = "-999999";
+    textLabel = new CSS2DObject(earthLabel);
+    textLabel.position.set(0, 5, 0);
+    scene.add(textLabel);
+
+    console.log(888,textLabel);
+    // 需要加上element
+    textLabel.element.addEventListener("click", function (e) {
+        console.log(e);
+    });
+
+   
 }
 
-
-function initShadow() {
-    renderer.shadowMap.enabled = true;
-    directionalLight.castShadow = true;
-}
 
 function initControls() {
-    // controls = new OrbitControls(camera, renderer.domElement);
-    controls = new OrbitControls(camera, labelRenderer.domElement);
-}
+    controls = new OrbitControls(camera, renderer.domElement);
+};
 
-window.addEventListener('resize', () => {
+
+window.addEventListener('resize', onWindowResize);
+
+function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    labelRenderer.setSize(window.innerWidth, window.innerHeight);
-})
+}
+
+
+
+
+function update() {
+    controls.update();
+};
 
 function animate() {
     elapsedTime = clock.getElapsedTime();
@@ -164,6 +199,7 @@ function animate() {
     labelRenderer.render(scene, camera);
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
+    update();
 
 }
 
